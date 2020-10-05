9e6f6a287c5b06f80701c088faeab6182b4f3773
Hector <hfigueroa37@gmail.com>
2020-10-05 00:40:08 -0700

    Updates

---
+++ precache-manifest.6981dd058d822b3afe48599c4f9b8429.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99ebb04b54cab9d41a5abfc38754533",
+    "url": "/crypto_tracker/index.html"
+  },
+  {
+    "revision": "7869384ecf9e733c0380",
+    "url": "/crypto_tracker/static/css/main.03ba0b97.chunk.css"
+  },
+  {
+    "revision": "e042156ecda685a4d548",
+    "url": "/crypto_tracker/static/js/2.86bab506.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crypto_tracker/static/js/2.86bab506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7869384ecf9e733c0380",
+    "url": "/crypto_tracker/static/js/main.990cb8b9.chunk.js"
+  },
+  {
+    "revision": "dc4dfb6ea481b6ae6c53",
+    "url": "/crypto_tracker/static/js/runtime-main.10ea711f.js"
+  }
+]);
